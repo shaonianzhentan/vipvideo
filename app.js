22a72be605d174732076cc3ce4a6d70fd5922a1c
zhulu <shaonianzhentan@sina.cn>
2018-09-28 09:59:06 +0800

    解决请求超时的问题

--- app.js
+++ app.js
@@ -4,38 +4,46 @@ class VipVideo {
     constructor() {
     }
     geturl(link) {
-        return new Promise((resolve, reject) => {             
+        return new Promise((resolve, reject) => {
             (async () => {
-              //const browser = await puppeteer.launch({headless: false,devtools: true,args:['--disable-web-security']});
-              const browser = await puppeteer.launch({args:['--no-sandbox', '--disable-setuid-sandbox','--disable-web-security']});
-              const page = await browser.newPage();
-              
-              page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1');
-              
-              //let link='https://www.mgtv.com/b/325599/4592004.html?cxid=90fe36tp1'
-              console.log(`请求地址：${link}`)
-              await page.goto(`http://yun.baiyug.cn/vip/?url=${link}`);
-              
-              dumpFrameTree(page.mainFrame(), '');
-              //await browser.close();
-            
-              async function dumpFrameTree(frame, indent) {
-                console.log(indent + frame.url());
-                
-                let videoHandle = await frame.$('video')
-                if(videoHandle != null){
-                    let video = await frame.evaluate(v => v.src, videoHandle);
-                    console.log(video)
-                    resolve(video)
-                    await browser.close();
+                //const browser = await puppeteer.launch({headless: false,devtools: true,args:['--disable-web-security']});
+                let browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-web-security'] });
+                const page = await browser.newPage();
+
+                page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1');
+
+                //let link='https://www.mgtv.com/b/325599/4592004.html?cxid=90fe36tp1'
+                console.log(`请求地址：${link}`)
+                await page.goto(`http://yun.baiyug.cn/vip/?url=${link}`);
+
+                dumpFrameTree(page.mainFrame(), '');
+                //await browser.close();
+
+                async function dumpFrameTree(frame, indent) {
+                    console.log(indent + frame.url());
+
+                    let videoHandle = await frame.$('video')
+                    if (videoHandle != null) {
+                        let video = await frame.evaluate(v => v.src, videoHandle);
+                        console.log(video)
+                        await browser.close();
+                        browser = null
+                        resolve(video)
+                    }
+
+                    for (let child of frame.childFrames())
+                        dumpFrameTree(child, indent + '  ');
                 }
-                
-                for (let child of frame.childFrames())
-                  dumpFrameTree(child, indent + '  ');
-              }    
-              //document.querySelector('iframe').contentWindow.document.querySelector('iframe').contentWindow.document.querySelector('iframe').contentWindow.document.querySelector('video').src
-             
-              //await browser.close();
+                //document.querySelector('iframe').contentWindow.document.querySelector('iframe').contentWindow.document.querySelector('iframe').contentWindow.document.querySelector('video').src
+
+                //超时30秒
+                setTimeout(() => {
+                    if (browser != null) {
+                        await browser.close();
+                        reject('请求超时')
+                    }
+                }, 30 * 1000)
+                //await browser.close();
             })();
             //https://v.qq.com/x/cover/fgqtuu38z91hfyw.html&type=qq
         })
