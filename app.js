fb1028c722d86a7bd881485f2b27e6d63fe02317
zhulu <shaonianzhentan@sina.cn>
2018-09-28 09:30:43 +0800

    解决不能在centos下启动的问题

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ class VipVideo {
         return new Promise((resolve, reject) => {             
             (async () => {
               //const browser = await puppeteer.launch({headless: false,devtools: true,args:['--disable-web-security']});
-              const browser = await puppeteer.launch({args:['--disable-web-security']});
+              const browser = await puppeteer.launch({args:['--no-sandbox', '--disable-setuid-sandbox','--disable-web-security']});
               const page = await browser.newPage();
               
               page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1');
